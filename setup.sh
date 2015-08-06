7a77b579f2d5c0925ae05b63e5ec918d6d24b6dc
thekelvinliu <kelvin@thekelvinliu.com>
2015-08-05 21:07:44 -0400

    added some aliases and re-arranged some exports

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # setup.sh - script to install homebrew and configure development environment
+# this is very-much-so a work in progress.
 
 if which brew | grep -q /usr/local/bin/brew; then
     echo homebrew is already installed
