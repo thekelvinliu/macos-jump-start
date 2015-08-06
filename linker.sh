6cd92ec25dde9f770f1f0f8750e9d4e02bb065c6
thekelvinliu <kelvin@thekelvinliu.com>
2015-08-06 16:20:28 -0400

    added script to symlink bash dotfiles to ~

---
+++ linker.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# linker.sh - script to symlink bash-related dotfiles in this repo to ~/
+
+# Expand a path to an absolute path
+function realpath {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
+# loop through bash-related dotfiles
+for file in ./dotfiles/bash/.[^.]*; do
+    if [ -r $file ]; then
+        file=$(realpath $file)
+        (cd $HOME && ln -Ffs $file)
+    fi
+done
+unset file
+
+# Source ~/.bash_profile
+. $HOME/.bash_profile
