6aef062877e54e6a50004b2e8acfe5803ef0344c
thekelvinliu <kelvin@thekelvinliu.com>
2015-08-06 17:26:01 -0400

    added xcode-select logic

--- setup.sh
+++ setup.sh
@@ -3,7 +3,12 @@
 # this is very-much-so a work in progress.
 
 if which brew | grep -q /usr/local/bin/brew; then
-    echo homebrew is already installed
+    echo "homebrew is already installed"
 else
-    echo homebrew is not installed, checking for xcode-select
+    echo "homebrew is not installed, checking for xcode-select"
+    if xcode-select -p 2>/dev/null; then
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    else
+        echo "install xcode before running this script!"
+    fi
 fi
