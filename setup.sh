1e182d943675ddd87e2e711fa95ef2781e621fe2
thekelvinliu <kelvin@thekelvinliu.com>
2015-08-13 22:05:46 -0500

    removed git clone stuff

--- setup.sh
+++ setup.sh
@@ -4,15 +4,11 @@
 
 #check for xcode
 if [[ -n $(xcode-select -p 2> /dev/null) ]]; then
-    #clone full git repo
-    cd $HOME
-    git clone https://github.com/thekelvinliu/osx-jump-start.git
+    #loop through scripts
+    for file in $HOME/osx-jump-start/scripts/{prompt.sh,linker.sh,ssh.sh,homebrew.sh,pip.sh,osx.sh}; do
+        [ -r "$file" ] && . "$file"
+    done
 else
     echo "Follow the instructions-- install Xcode and agree to the liscence before doing this!"
 fi
-
-#loop through scripts
-for file in $HOME/osx-jump-start/scripts/{prompt.sh,linker.sh,ssh.sh,homebrew.sh,pip.sh,osx.sh}; do
-    [ -r "$file" ] && . "$file"
-done
 unset file un email
