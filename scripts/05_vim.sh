4f98212ba17356938b1c3c09853d120394dfd868
Kelvin Liu <kelvinliu@Kelvins-Macbook-Air.local>
2015-08-20 16:13:11 -0500

    finished script to get vim installed

--- scripts/05_vim.sh
+++ scripts/05_vim.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # 05_vim.sh - script to install vim, pathogen, and some plugins
+# Assumes homebrew is already installed. If not, run 04_homebrew.sh
 
 # Installs a homebrew formula
 function brew_install {
@@ -15,61 +16,28 @@ function brew_installed {
     brew list -1 | fgrep -qx $1
 }
 
-# Taps a homebrew repo
-function brew_tap {
-    if brew_tapped $1; then
-        echo "$GREEN$1$RESET is already tapped."
-    else
-        brew tap $1
-    fi
-}
-
-# Checks whether given repo is already tapped
-function brew_tapped {
-    brew tap | fgrep -qx $1
-}
-
-if [[ -n $(which brew) ]]; then
-    echo "homebrew is already installed"
-else
-    echo "homebrew is not installed-- installing now!"
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
-
-#check doctor
-brew doctor
-
 #update formulae
 brew update
 
-#add other repos
-brew_tap homebrew/python
-brew_tap caskroom/cask
-brew_tap caskroom/versions
+#install vim
+brew_install vim
 
-#basics
-brew_install coreutils
-brew_install findutils
-brew_install git
-brew_install htop
-brew_install tree
-brew_install ffmpeg
-brew_install imagemagick
+#install pathogen
+mkdir -p $HOME/.vim/autoload $HOME/.vim/bundle
+if [[ ! -e $HOME/.vim/autoload/pathogen.vim ]]; then
+    curl -LSso $HOME/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+fi
 
-#python
-brew_install python
-brew_install python3
-brew linkapps
-brew_install "numpy --with-python3"
-brew_install "scipy --with-python3"
-brew_install "matplotlib --with-python3"
-brew_install "pygame --with-python3"
+#install plugins
+#vim-sensible
+if [[ ! -e $HOME/.vim/bundle/vim-sensible ]]; then
+    git clone https://github.com/tpope/vim-sensible.git $HOME/.vim/bundle/vim-sensible
+fi
 
-#web
-brew_install node
-brew_install heroku-toolbelt
-brew_install mysql
-brew_install mongodb
+#auto-pairs
+if [[ ! -e $HOME/.vim/bundle/auto-pairs ]]; then
+    git clone https://github.com/jiangmiao/auto-pairs.git $HOME/.vim/bundle/auto-pairs
+fi
 
 #clean things up
 brew cleanup
