9e97247bb06dc8dc6cdcc2b96a288c74ced1a21d
thekelvinliu <kelvin@thekelvinliu.com>
2015-08-20 17:07:04 -0500

    added colors and some messages

--- setup.sh
+++ setup.sh
@@ -4,11 +4,15 @@
 
 #check for xcode
 if [[ -n $(xcode-select -p 2> /dev/null) ]]; then
+    #get colors
+    . $HOME/osx-jump-start/dotfiles/.bash_profile
+
     #loop through scripts
     for file in $HOME/osx-jump-start/scripts/[0-9]*; do
-        echo $file
+        echo $'\nRunning'$ORANGE$file$RESET && . $file && sleep 2
     done
 else
     echo "Follow the instructions-- install Xcode and agree to the liscence before doing this!"
 fi
 unset file un email
+echo $GREEN$BOLD$'\nD O N E'$RESET" -- Enjoy developing on your new system! -- "$GREEN$BOLD$'D O N E\n'$RESET
