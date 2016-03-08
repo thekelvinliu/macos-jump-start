7cc1342f291764ea7cf01604d07a69b0a0269c2c
thekelvinliu <kelvin@thekelvinliu.com>
2016-03-08 20:07:03 +0000

    added working code

--- scripts/09_sublime.sh
+++ scripts/09_sublime.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 # sublime.sh - symlink sublime user settings and color scheme
 # inspired by https://raw.githubusercontent.com/mathiasbynens/dotfiles/master/.osx
+
+# Expand a path to an absolute path
+function realpath {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
+#change to sublime dir
+cd "$HOME/Library/Application Support/Sublime Text 3/Packages"
+
+#theme
+ln -Ffs $HOME/osx-jump-start/configs/sublime/Dracula.tmTheme
+#settings
+cd "User"
+ln -Ffs $HOME/osx-jump-start/configs/sublime/Preferences.sublime-settings
