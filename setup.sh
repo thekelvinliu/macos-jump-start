254bd55ca65638ccb5c590e1a897e3e8df633fd2
thekelvinliu <kelvin@thekelvinliu.com>
2016-03-08 20:00:00 +0000

    change ORANGE to YELLOW

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ if [[ -n $(xcode-select -p 2> /dev/null) ]]; then
 
     #loop through scripts
     for file in $HOME/osx-jump-start/scripts/[0-9]*; do
-        echo $'\nRunning'$ORANGE$file$RESET && . $file && sleep 2
+        echo $'\nRunning'$YELLOW$file$RESET && . $file && sleep 2
     done
 else
     echo "Follow the instructions-- install Xcode and agree to the liscence before doing this!"
