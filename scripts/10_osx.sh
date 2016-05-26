4e40943a3f48391c9e2010aaf1214445af6b2b96
thekelvinliu <kelvin@thekelvinliu.com>
2016-05-26 12:35:14 -0500

    added line to prevent .DS_Store files on network drives

--- scripts/10_osx.sh
+++ scripts/10_osx.sh
@@ -5,5 +5,11 @@
 #show hidden files in finder
 defaults write com.apple.finder AppleShowAllFiles -bool true
 
+#don't write .DS_Store to network drives
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
 #fast key repeat
 defaults write NSGlobalDomain KeyRepeat -int 0
+
+#kill existing finder sessions
+killall Finder
