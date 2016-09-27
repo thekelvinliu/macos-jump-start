b39fb51f4f565e1cd5d8c8ec4e2fc03642ea673d
thekelvinliu <kelvin@thekelvinliu.com>
2016-09-27 15:05:39 +0400

    reorganized brew cask apps

--- scripts/07_cask.sh
+++ scripts/07_cask.sh
@@ -23,47 +23,53 @@ brew update
 brew_install brew-cask
 brew cask update
 
-# install!
-# important
-brew_cask_install dropbox
-brew_cask_install google-chrome
+# install apps in order determined by me
+# important apps
 brew_cask_install iterm2
 brew_cask_install sublime-text
+brew_cask_install google-chrome
+brew_cask_install dropbox
+# brew_cask_install slack
 
-# development
+# development-realted
 brew_cask_install java
 brew_cask_install xquartz
-brew_cask_install firefoxdevelopmentedition
-
-# general
-
-
-brew_cask_install skim
+brew_cask_install osxfuse
+brew_cask_install ngrok
+brew_cask_install firefoxdeveloperedition
+brew_cask_install safari-technology-preview
+brew_cask_install sequel-pro
+brew_cask_install robomongo
 brew_cask_install virtualbox
 brew_cask_install virtualbox-extension-pack
 # brew_cask_install vagrant
-brew_cask_install flux
-brew_cask_install sequel-pro
-brew_cask_install robomongo
+# brew_cask_install rstudio
+
+# general apps
 brew_cask_install appcleaner
-brew_cask_install disk-inventory-x
 brew_cask_install coconutbattery
-brew_cask_install skype
-# brew_cask_install slack
 brew_cask_install cyberduck
-# brew_cask_install handbrake
+brew_cask_install disk-inventory-x
+brew_cask_install evernote
+brew_cask_install flux
+brew_cask_install skim
+brew_cask_install skype
+brew_cask_install the-unarchiver
+# brew_cask_install torbrowser
+
+# media apps
 brew_cask_install filebot
 brew_cask_install transmission
 brew_cask_install vlc
-brew_cask_install steam
-brew_cask_install evernote
-brew_cask_install the-unarchiver
-# brew_cask_install rstudio
-brew_cask_install minecraft
-brew_cask_install imageoptim
-brew_cask_install imagealpha
 brew_cask_install vox
 brew_cask_install vox-preferences-pane
+# brew_cask_install handbrake
+brew_cask_install imageoptim
+brew_cask_install imagealpha
+
+# games
+brew_cask_install minecraft
+brew_cask_install steam
 
 #clean things up
 brew cask cleanup
