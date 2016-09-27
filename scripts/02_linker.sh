eebe84231f4f9303a34ae886fb5ea43c1268cd5a
thekelvinliu <kelvin@thekelvinliu>
2016-09-27 21:42:02 +0400

    provide access to functions and fixed typo

--- scripts/02_linker.sh
+++ scripts/02_linker.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 # 02_linker.sh - script to symlink bash-related dotfiles in this repo to ~/
 
-# Expand a path to an absolute path
-function realpath {
-    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
-}
+# enable aliases and functions
+. $HOME/osx-jump-start/dotfiles/.functions
 
 # check for $un and $email, if either is undefined, run 01_prompt.sh
 if [[ -z $un || -z $email ]]; then
-    echo "Missing username and/or password!"
+    echo "Missing username and/or email!"
     . $HOME/osx-jump-start/scripts/01_prompt.sh
 fi
 
@@ -21,10 +19,8 @@ for file in $HOME/osx-jump-start/dotfiles/.[^.]*; do
             cat $file | sed "s#_un#$un#g;s#_email#$email#g;s#_HOME#$HOME#g" > $HOME/.gitconfig
         # otherwise, just symlink
         else
-            (cd $HOME && ln -Ffs $file)
+            (cd && ln -Ffs $file)
         fi
     fi
 done
 unset file
-. $HOME/.bash_profile && echo $PURPLE"sourced ~/.bash_profile"$RESET
-echo $PURPLE"dotfiles linked."$RESET
