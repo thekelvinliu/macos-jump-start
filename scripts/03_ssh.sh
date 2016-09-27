24990d86d950327b1f1cf9eb08951c5bde6021b0
thekelvinliu <kelvin@thekelvinliu.com>
2016-09-27 15:45:02 +0400

    generate an ed25519 ssh keypair by default

--- scripts/03_ssh.sh
+++ scripts/03_ssh.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # 03_ssh.sh - creates ssh keys if none exist
 
-if [[ ! -e ${SSH_KEY:=$HOME/.ssh/id_rsa} ]]; then
-    ssh-keygen -t rsa -b 4096 -C $email -N "" -f $SSH_KEY -q
-    eval "$(ssh-agent -s)"
-    ssh-add $SSH_KEY
+if [[ ! -e ${SSH_PUBLIC_KEY:=$HOME/.ssh/id_ed25519.pub} ]]; then
+    ssh-keygen -t ed25519 -a 100 -C $email -f $SSH_PRIVATE_KEY
+    # eval "$(ssh-agent -s)"
+    # ssh-add $SSH_KEY
     # pbcopy < $SSH_KEY
     # open https://github.com/settings/ssh
 else
-    echo $SSH_KEY already exists.
+    echo "$SSH_KEY already exists."
 fi
