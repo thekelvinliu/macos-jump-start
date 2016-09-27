c19f0caa59af34d06313ed59fae609c0dffa22c0
thekelvinliu <kelvin@thekelvinliu>
2016-09-28 00:50:16 +0400

    full ssh script!

--- scripts/03_ssh.sh
+++ scripts/03_ssh.sh
@@ -2,11 +2,13 @@
 # 03_ssh.sh - creates ssh keys if none exist
 
 if [[ ! -e ${SSH_PUBLIC_KEY:=$HOME/.ssh/id_ed25519.pub} ]]; then
+    # generate an Ed25519 key
     ssh-keygen -t ed25519 -a 100 -C $email -f $SSH_PRIVATE_KEY
-    # eval "$(ssh-agent -s)"
-    # ssh-add $SSH_KEY
-    # pbcopy < $SSH_KEY
-    # open https://github.com/settings/ssh
+    # let osx keychain handle the key
+    ssh-add -K $SSH_PRIVATE_KEY
+    # copy public key to clipboard and open github
+    pbcopy < $SSH_PUBLIC_KEY
+    open https://github.com/settings/ssh
 else
     echo "$SSH_KEY already exists."
 fi
