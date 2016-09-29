6fce9460edb9c8c758c6cbc40e08ea5c8450102c
thekelvinliu <kelvin@thekelvinliu.com>
2016-09-29 15:10:56 +0400

    added message about adding to osx keychain

--- scripts/03_ssh.sh
+++ scripts/03_ssh.sh
@@ -8,6 +8,7 @@ if [[ ! -e $SSH_PRIVATE_KEY || ! -e $SSH_PUBLIC_KEY ]]; then
     # generate an Ed25519 key
     ssh-keygen -t ed25519 -a 100 -C $email -f $SSH_PRIVATE_KEY
     # let osx keychain handle the key
+    echo "adding ssh key to keychain -- you may need to enter your password"
     ssh-add -K $SSH_PRIVATE_KEY
     # copy public key to clipboard and open github
     pbcopy < $SSH_PUBLIC_KEY
