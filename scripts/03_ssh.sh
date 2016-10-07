a7e33ef3d4edf0492e9e1ecb5371f34556405227
thekelvinliu <kelvin@thekelvinliu.com>
2016-10-07 23:50:28 +0400

    ask for email if necessary

--- scripts/03_ssh.sh
+++ scripts/03_ssh.sh
@@ -4,6 +4,12 @@
 # ensure $SSH_PRIVATE_KEY and $SSH_PUBLIC_KEY are set
 . $HOME/osx-jump-start/dotfiles/.exports
 
+# check for $email and run 01_prompt.sh if it is undefined
+if [[ -z $email ]]; then
+    echo "Missing email!"
+    . $HOME/osx-jump-start/scripts/01_prompt.sh
+fi
+
 if [[ ! -e $SSH_PRIVATE_KEY || ! -e $SSH_PUBLIC_KEY ]]; then
     # generate an Ed25519 key
     ssh-keygen -t ed25519 -a 100 -C $email -f $SSH_PRIVATE_KEY
