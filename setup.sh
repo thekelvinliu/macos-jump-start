60c048cb5108829d73a24709fe2c218b759367f5
thekelvinliu <kelvin@thekelvinliu.com>
2017-07-24 16:07:50 -0700

    cleaning up setup script

--- setup.sh
+++ setup.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/env bash
 # setup.sh - sets up scripts and dotfiles for a happy development environment
 
+# some variables
+DONE="$GREEN$BOLD d o n e $RESET"
+OJS="$HOME/osx-jump-start"
+
 # ensure xcode is installed
 if [[ -n $(xcode-select -p 2> /dev/null) ]]; then
   # enable colors
-  . "$HOME/osx-jump-start/dotfiles/.bash_prompt"
+  . "$OJS/dotfiles/.bash_prompt"
 
   # run the scripts
-  for file in "$HOME"/osx-jump-start/scripts/[0-9]*; do
-    echo "Running $YELLOW$file$RESET..."
+  for file in "$OJS/scripts/"[0-9]*; do
+    echo "running $YELLOW$file$RESET..."
     # . $file && sleep 1
   done
 else
-  echo "Install Xcode and agree to the liscence before running this script!"
+  echo "install xcode and agree to the liscence before running this script."
 fi
 
-echo "$GREEN$BOLD D O N E $RESET"
-echo "-- Enjoy developing on your new system! --"
-echo "$GREEN$BOLD D O N E $RESET"
+echo "$DONE"
+echo "system ready -- enjoy development!"
+echo "$DONE"
+
+unset DONE OJS
