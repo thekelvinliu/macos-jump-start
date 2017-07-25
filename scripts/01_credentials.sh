4f9449946cb9940d5318ecceb8e10e495d2a4a75
thekelvinliu <kelvin@thekelvinliu.com>
2017-07-25 16:25:29 -0700

    set and export actual environment variables

--- scripts/01_credentials.sh
+++ scripts/01_credentials.sh
@@ -1,21 +1,26 @@
 #!/usr/bin/env bash
-# 01_prompt.sh - script to get github username and email
+# sets git email and username environment variables
 
 while true; do
-  read -p "Enter your github username: " un
-  read -p "Your github username is $GREEN$un$RESET. Is this correct-- yes or no? " yn
-  case $yn in
-    [Yy]* ) break;;
-    [Nn]* ) :;;
-    * ) echo "Please answer yes or no.";;
+  read -p "enter your git ${YELLOW}email$RESET: " GIT_EMAIL
+  echo "your git ${YELLOW}email$RESET is $GREEN$GIT_EMAIL$RESET"
+  read -p "is that correct? (y/n) " yn
+  case "$yn" in
+    [Yy]*) break;;
+    [Nn]*) :;;
+    *) echo "please answer yes or no.";;
   esac
 done
+
 while true; do
-  read -p "Enter your email: " email
-  read -p "Your email is $GREEN$email$RESET. Is this correct-- yes or no? " yn
-  case $yn in
-    [Yy]* ) break;;
-    [Nn]* ) :;;
-    * ) echo "Please answer yes or no.";;
+  read -p "enter your git ${YELLOW}username$RESET: " GIT_USERNAME
+  echo "your git ${YELLOW}username$RESET is $GREEN$GIT_USERNAME$RESET"
+  read -p "is that correct? (y/n) " yn
+  case "$yn" in
+    [Yy]*) break;;
+    [Nn]*) :;;
+    *) echo "please answer yes or no.";;
   esac
 done
+
+export GIT_{EMAIL,USERNAME}
