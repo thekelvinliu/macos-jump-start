acb3e0ea61ed5b26a05b440df78b869d64ede67f
thekelvinliu <kelvin@thekelvinliu.com>
2017-07-24 23:02:00 -0700

    change to python-specific script

--- scripts/08_python.sh
+++ scripts/08_python.sh
@@ -16,3 +16,32 @@ fi
 pip_install ipython && pip3_install ipython
 pip_install requests && pip3_install requests
 # pip_install Flask
+
+
+# checks whether a given library is already installed with pip2
+function pip2_installed {
+  pip2 show "$1" &> /dev/null
+}
+
+# installs a python library with pip2
+function pip2_install {
+  if pip2_installed "$1"; then
+    echo "$BLUE$1$RESET is already installed."
+  else
+    pip2 install "$1"
+  fi
+}
+
+# checks whether a given library is already installed with pip3
+function pip3_installed {
+  pip3 show "$1" &> /dev/null
+}
+
+# installs a python library with pip3
+function pip3_install {
+  if pip3_installed "$1"; then
+    echo "$BLUE$1$RESET is already installed."
+  else
+    pip3 install "$1"
+  fi
+}
