b91c8a608ca92297fa1eee587b95d46ec8df0282
thekelvinliu <kelvin@thekelvinliu.com>
2017-07-25 19:25:04 -0700

    updated code to work with GIT_EMAIL

--- scripts/03_ssh.sh
+++ scripts/03_ssh.sh
@@ -1,24 +1,24 @@
 # creates a strong default ssh keypair
 
-# ensure $SSH_PRIVATE_KEY and $SSH_PUBLIC_KEY are set
-. $HOME/osx-jump-start/dotfiles/.exports
-
-# check for $email and run 01_prompt.sh if it is undefined
-if [[ -z "$email" ]]; then
-  echo "Missing email!"
-  . $HOME/osx-jump-start/scripts/01_prompt.sh
-fi
+# ensure SSH_PRIVATE_KEY and SSH_PUBLIC_KEY are set
+OJS=${OJS:-"$HOME/osx-jump-start"}
+. "$OJS/dotfiles/.exports"
 
 # generate a key if necessary
-if [[ ! -e $SSH_PRIVATE_KEY || ! -e $SSH_PUBLIC_KEY ]]; then
-  # generate an Ed25519 key
-  ssh-keygen -t ed25519 -a 100 -C $email -f $SSH_PRIVATE_KEY
+if [[ -e "$SSH_PRIVATE_KEY" && -e "$SSH_PUBLIC_KEY" ]]; then
+  echo "an ed25519 keypair already exists."
+else
+  # check for GIT_EMAIL
+  if [[ -z "$GIT_EMAIL" ]]; then
+    echo "missing git email"
+    . "$OJS/scripts/01_credentials.sh"
+  fi
+  # generate an ed25519 key
+  ssh-keygen -t ed25519 -a 100 -C "$GIT_EMAIL" -f "$SSH_PRIVATE_KEY"
   # let osx keychain handle the key
   echo "adding ssh key to keychain -- you may need to enter your password"
-  ssh-add -K $SSH_PRIVATE_KEY
+  ssh-add -K "$SSH_PRIVATE_KEY"
   # copy public key to clipboard and open github
-  pbcopy < $SSH_PUBLIC_KEY
+  pbcopy < "$SSH_PUBLIC_KEY"
   open https://github.com/settings/ssh
-else
-  echo "an Ed25519 keypair already exists."
 fi
