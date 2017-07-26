36279cf257ed6cd5e0a6ee84ad784e798b8096aa
thekelvinliu <kelvin@thekelvinliu.com>
2017-07-25 22:34:07 -0700

    added some color

--- scripts/08_vim.sh
+++ scripts/08_vim.sh
@@ -20,17 +20,23 @@ brew_install vim
 mkdir -p "$HOME/.vim/autoload" "$HOME/.vim/bundle"
 if [[ ! -e "$HOME/.vim/autoload/pathogen.vim" ]]; then
   curl -LSso "$HOME/.vim/autoload/pathogen.vim" https://tpo.pe/pathogen.vim
+else
+  echo "${BLUE}pathogen$RESET is already installed."
 fi
 
 # install plugins
-# vim-sensible
-if [[ ! -e "$HOME/.vim/bundle/vim-sensible" ]]; then
-  git clone https://github.com/tpope/vim-sensible.git "$HOME/.vim/bundle/vim-sensible"
-fi
-
 # auto-pairs
 if [[ ! -e "$HOME/.vim/bundle/auto-pairs" ]]; then
   git clone https://github.com/jiangmiao/auto-pairs.git "$HOME/.vim/bundle/auto-pairs"
+else
+  echo "${BLUE}auto-pairs$RESET is already installed."
+fi
+
+# vim-sensible
+if [[ ! -e "$HOME/.vim/bundle/vim-sensible" ]]; then
+  git clone https://github.com/tpope/vim-sensible.git "$HOME/.vim/bundle/vim-sensible"
+else
+  echo "${BLUE}vim-sensible$RESET is already installed."
 fi
 
 # clean things up
