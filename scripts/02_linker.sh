9760ff5b4fbe1fa037a01b3b3dc205557d34a00a
thekelvinliu <kelvin@thekelvinliu.com>
2017-07-25 18:51:58 -0700

    changed to support new GIT_EMAIL and GIT_USERNAME variables

--- scripts/02_linker.sh
+++ scripts/02_linker.sh
@@ -1,31 +1,24 @@
 # symlinks dotfiles to $HOME
 
-# get a common execution environment
-. "$OJS/common.sh"
 # enable functions
-. "$OJS"
+OJS=${OJS:-"$HOME/osx-jump-start"}
+. "$OJS/dotfiles/.functions"
 
-#
-# enable functions
-. $HOME/osx-jump-start/dotfiles/.functions
-
-# check for $un and $email, if either is undefined, run 01_prompt.sh
-if [[ -z "$un" || -z "$email" ]]; then
-  echo "Missing username and/or email!"
-  . $HOME/osx-jump-start/scripts/01_prompt.sh
+# check for GIT_EMAIL and GIT_USERNAME
+if [[ -z "$GIT_EMAIL" || -z "$GIT_USERNAME" ]]; then
+  echo "missing git email and/or username"
+  . "$OJS/scripts/01_credentials.sh"
 fi
 
-# loop through bash-related dotfiles
-for file in "$HOME"/osx-jump-start/dotfiles/.[^.]*; do
-  if [ -r "$file" ]; then
-    file=$(realpath $file)
-    # insert username and email into gitconfig
-    if [[ $file =~ .gitconfig ]]; then
-      cat $file | sed "s#_un#$un#g;s#_email#$email#g;s#_HOME#$HOME#g" > $HOME/.gitconfig
-    # otherwise, just symlink
-    else
-      (cd && ln -Ffs $file)
-    fi
+# loop over dotfiles directory
+for dot in "$OJS/dotfiles/".[^.]*; do
+  # ensure readable
+  [[ ! -r "$dot" ]] && continue
+  # insert email and username into .gitconfig
+  if [[ "$dot" =~ .gitconfig ]]; then
+    cat "$dot" | sed "s#_USERNAME#$GIT_USERNAME#g;s#_EMAIL#$GIT_EMAIL#g;s#_HOME#$HOME#g" > "$HOME/.gitconfig"
+  else
+    (cd && ln -Ffs "$dot")
   fi
 done
-unset file
+unset dot
