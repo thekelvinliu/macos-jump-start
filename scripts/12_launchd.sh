6af6f618ff1f7f193d80aea700f751d2b4ae48b9
thekelvinliu <kelvin@thekelvinliu.com>
2017-11-21 22:14:01 -0600

    finish symlinks in launchd script

--- scripts/12_launchd.sh
+++ scripts/12_launchd.sh
@@ -7,19 +7,24 @@ MJS_BASE=${MJS_BASE:-"$HOME/macos-jump-start"}
 # setup
 agents="$HOME/Library/LaunchAgents"
 mkdir -p "$agents"
+daemons="/Library/LaunchDaemons"
 base="$MJS_BASE/configs/launchd"
 
 # symlink launch agents
-for plist in com.thekelvinliu.{gdfs-defaults,launch-alfred,ssh-adder}.plist; do
+for plist in com.thekelvinliu.{backup-{home,thin},gdfs-defaults,launch-alfred,ssh-adder}.plist; do
   launchctl unload "$agents/$plist" 2> /dev/null
   ln -Ffs "$base/$plist" "$agents"
   echo "symlinked $MAGENTA$plist$RESET to $CYAN$agents$RESET"
   launchctl load "$agents/$plist"
 done
 
-# TODO write symlink launch daemons
-# for plist in com.thekelvinliu.{}.plist; do
-# done
+# symlink backup-root as launch daemon for root
+plist=com.thekelvinliu.backup-root.plist
+sudo launchctl unload "$daemons/$plist" 2> /dev/null
+sudo ln -Ffs "$base/$plist" "$daemons"
+sudo chown root:wheel "$daemons/$plist"
+echo "symlinked $MAGENTA$plist$RESET to $CYAN$daemons$RESET"
+sudo launchctl load "$daemons/$plist"
 
 # remove variables
-unset agents base plist
+unset agents daemons base plist
