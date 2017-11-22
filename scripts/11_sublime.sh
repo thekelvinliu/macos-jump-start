4a4cf01505388f3309faec37bbb7f757f482d1d8
thekelvinliu <kelvin@thekelvinliu.com>
2017-11-21 19:12:56 -0600

    create sublime symlinks in a subshell

--- scripts/11_sublime.sh
+++ scripts/11_sublime.sh
@@ -1,19 +1,18 @@
-# symlinks sublime user settings and color scheme
+# symlinks sublime user settings
 
 # get a common execution environment
 MJS_BASE=${MJS_BASE:-"$HOME/macos-jump-start"}
 . "$MJS_BASE/common.sh"
 
-# navigate to sublime directory
-sublime="$HOME/Library/Application Support/Sublime Text 3/Packages/User"
-mkdir -p "$sublime"
-cd "$sublime"
+# using a subshell
+(
+  # navigate to sublime directory
+  sublime="$HOME/Library/Application Support/Sublime Text 3/Packages/User"
+  mkdir -p "$sublime"
+  cd "$sublime"
 
-# symlink settings files
-for s in "$MJS_BASE/configs/sublime/"*.sublime-*; do
-  ln -Ffs "$s"
-done
-
-# reset
-unset sublime s
-cd - > /dev/null
+  # symlink settings files
+  for s in "$MJS_BASE/configs/sublime"/*; done
+    ln -Ffs "$s"
+  done
+)
