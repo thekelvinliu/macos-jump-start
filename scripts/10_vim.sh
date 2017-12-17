46de5afd5452b9939f8aa3c0bb64f0c882fc7fe1
thekelvinliu <kelvin@thekelvinliu.com>
2017-12-16 18:22:51 -0800

    remove pathogen stuff

--- scripts/10_vim.sh
+++ scripts/10_vim.sh
@@ -16,28 +16,5 @@ brew update
 # install vim
 brew_install vim
 
-# install pathogen
-mkdir -p "$HOME/.vim/autoload" "$HOME/.vim/bundle"
-if [[ ! -e "$HOME/.vim/autoload/pathogen.vim" ]]; then
-  curl -LSso "$HOME/.vim/autoload/pathogen.vim" https://tpo.pe/pathogen.vim
-else
-  echo "${BLUE}pathogen$RESET is already installed."
-fi
-
-# install plugins
-# auto-pairs
-if [[ ! -e "$HOME/.vim/bundle/auto-pairs" ]]; then
-  git clone https://github.com/jiangmiao/auto-pairs.git "$HOME/.vim/bundle/auto-pairs"
-else
-  echo "${BLUE}auto-pairs$RESET is already installed."
-fi
-
-# vim-sensible
-if [[ ! -e "$HOME/.vim/bundle/vim-sensible" ]]; then
-  git clone https://github.com/tpope/vim-sensible.git "$HOME/.vim/bundle/vim-sensible"
-else
-  echo "${BLUE}vim-sensible$RESET is already installed."
-fi
-
 # clean things up
 brew cleanup
