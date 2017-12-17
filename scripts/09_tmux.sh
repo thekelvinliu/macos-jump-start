1ac3a967b69b0b11b0dcb3aab04311c7b2dfab0a
thekelvinliu <kelvin@thekelvinliu.com>
2017-12-16 17:33:00 -0800

    finish tmux install script

--- scripts/09_tmux.sh
+++ scripts/09_tmux.sh
@@ -3,3 +3,18 @@
 # get a common execution environment
 MJS_BASE=${MJS_BASE:-"$HOME/macos-jump-start"}
 . "$MJS_BASE/common.sh"
+
+# ensure homebrew is installed before continuing
+if ! has_brew; then
+  echo "homebrew is not installed -- exiting."
+  return
+fi
+
+# update formulae
+brew update
+
+# copy to system clipboard
+brew_install reattach-to-user-namespace
+
+# tmux
+brew_install tmux
