3caf0392df90417d2945169cd79df7847fe34097
thekelvinliu <kelvin@thekelvinliu.com>
2018-01-13 21:05:27 -0800

    calculate uptime based on boot time and current time

--- configs/tmuxline/uptime.sh
+++ configs/tmuxline/uptime.sh
@@ -1,20 +1,12 @@
 # print system uptime nicely
 
-uptime | awk -F'[ ,:\t\n]+' '{
-  msg = "↑ "
-  if ($5 == "day" || $5 == "days") {
-    msg = msg $4"d "
-    n = $6
-    o = $7
-  } else {
-    n = $4
-    o = $5
-  }
-  if (int(o) == 0) {
-    msg = msg int(n)" "o
-  } else {
-    msg = msg int(n)"h "
-    msg = msg int(o)"m"
-  }
-  print msg
-}'
+s_to_dhms() {
+  ((d=${1}/86400))
+  ((h=(${1}%86400)/3600))
+  ((m=(${1}%3600)/60))
+  printf "↑ %dd %dh %dm\n" "$d" "$h" "$m"
+}
+
+bt=$(sysctl -n kern.boottime | awk -F'[=,}]' '{print $2}')
+ct=$(date +%s)
+s_to_dhms "$(($ct - $bt))"
