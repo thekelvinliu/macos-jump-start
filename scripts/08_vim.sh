3b6abf3bea1ad172f3b9b56492b67e9931583183
thekelvinliu <kelvin@thekelvinliu.com>
2018-01-14 14:38:00 -0800

    ensure init.vim is writable

--- scripts/08_vim.sh
+++ scripts/08_vim.sh
@@ -22,7 +22,9 @@ pip2_install neovim
 pip3_install neovim
 npm i -g neovim
 # write config
-cat > "$HOME/.config/nvim/init.vim" <<EOF
+nvim_path=$HOME/.config/nvim
+mkdir -p "$nvim_path"
+cat > "$nvim_path/init.vim" <<EOF
 set runtimepath^=~/.vim runtimepath+=~/.vim/after
 let &packpath = &runtimepath
 source ~/.vimrc
