246b8fa37352b9af19fb126848c8a1810576f7c4
thekelvinliu <kelvin@thekelvinliu.com>
2018-02-25 01:19:41 -0800

    create sshd files and add logic to symlink

--- scripts/04_ssh.sh
+++ scripts/04_ssh.sh
@@ -1,8 +1,13 @@
-# create a strong default ssh keypair
+# install homebrew ssh and create a strong keypair
 
-# ensure SSH_PRIVATE_KEY and SSH_PUBLIC_KEY are set
+# get a common execution environment
 MJS_BASE=${MJS_BASE:-"$HOME/macos-jump-start"}
+. "$MJS_BASE/common.sh"
+# ensure SSH_PRIVATE_KEY and SSH_PUBLIC_KEY are set
 . "$MJS_BASE/dotfiles/.exports"
+
+# openssh
+brew_install openssh
 
 # generate a key if necessary
 if [[ -e "$SSH_PRIVATE_KEY" && -e "$SSH_PUBLIC_KEY" ]]; then
@@ -19,3 +24,23 @@ else
   pbcopy < "$SSH_PUBLIC_KEY"
   echo "$SSH_PUBLIC_KEY was copied to the clipboard"
 fi
+
+# symlink sshd_config
+fname=/etc/ssh/sshd_config
+echo "requesting password to symlink sshd_config"
+[[ -f $fname ]] && sudo mv "$fname" "$fname.old"
+sudo ln -Ffs "$MJS_BASE/configs/ssh/sshd_config" "$fname"
+
+# symlink launchd plists
+daemons="/Library/LaunchDaemons"
+sudo mkdir -p "$daemons"
+plist="$MJS_BASE/configs/ssh/sshd.plist"
+fname=$(basename "$plist")
+fout=$daemons/$fname
+sudo launchctl unload "$fout" 2> /dev/null
+sudo ln -Ffs "$plist" "$daemons"
+echo "symlinked $MAGENTA$fname$RESET to $CYAN$fout$RESET"
+sudo launchctl load "$fout"
+
+# remove variables
+unset daemons fname fout
