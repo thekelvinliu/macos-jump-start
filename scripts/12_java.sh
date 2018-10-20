7ff1e7f99c11b940e342e8f0b0c1d2d92bb982a6
thekelvinliu <kelvin@thekelvinliu.com>
2018-10-20 12:04:32 -0700

    comment out java stuff for now

--- scripts/12_java.sh
+++ scripts/12_java.sh
@@ -10,20 +10,20 @@ if ! has_brew; then
   return
 fi
 
-# update formulae
-brew update
-
-# jenv
-brew_install jenv
-
-# java 9
-brew_cask_install java
-jenv add "$(/usr/libexec/java_home -v 9.0)"
-
-# java 8
-brew_cask_install java8
-jenv add "$(/usr/libexec/java_home -v 1.8)"
-jenv global 9.0
-
-# gradle
-brew_install gradle
+# # update formulae
+# brew update
+#
+# # jenv
+# brew_install jenv
+#
+# # java 9
+# brew_cask_install java
+# jenv add "$(/usr/libexec/java_home -v 9.0)"
+#
+# # java 8
+# brew_cask_install java8
+# jenv add "$(/usr/libexec/java_home -v 1.8)"
+# jenv global 9.0
+#
+# # gradle
+# brew_install gradle
